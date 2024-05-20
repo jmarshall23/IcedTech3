5d3f1c2815f408211a980d540d82de88f15257f5
Justin Marshall <justinmarshall20@gmail.com>
2024-05-19 19:42:03 -0700

    Fixed a bug were idMoveable props weren't reacting to physics.

--- neo/game/Moveable.cpp
+++ neo/game/Moveable.cpp
@@ -95,16 +95,43 @@ void idMoveable::Spawn( void ) {
 	float density, friction, bouncyness, mass;
 	int clipShrink;
 	idStr clipModelName;
+	bool setClipModel = false;
+	idBounds bounds;
 
 	// check if a clip model is set
-	spawnArgs.GetString( "clipmodel", "", clipModelName );
-	if ( !clipModelName[0] ) {
-		clipModelName = spawnArgs.GetString( "model" );		// use the visual model
+	spawnArgs.GetString("clipmodel", "", clipModelName);
+	if (!clipModelName[0]) {
+		idVec3 size;
+		if (spawnArgs.GetVector("mins", NULL, bounds[0]) &&
+			spawnArgs.GetVector("maxs", NULL, bounds[1])) {
+			setClipModel = true;
+			if (bounds[0][0] > bounds[1][0] || bounds[0][1] > bounds[1][1] || bounds[0][2] > bounds[1][2]) {
+				gameLocal.Error("Invalid bounds '%s'-'%s' on moveable '%s'", bounds[0].ToString(), bounds[1].ToString(), name.c_str());
+			}
+		}
+		else if (spawnArgs.GetVector("size", NULL, size)) {
+			if ((size.x < 0.0f) || (size.y < 0.0f) || (size.z < 0.0f)) {
+				gameLocal.Error("Invalid size '%s' on moveable '%s'", size.ToString(), name.c_str());
+			}
+			bounds[0].Set(size.x * -0.5f, size.y * -0.5f, 0.0f);
+			bounds[1].Set(size.x * 0.5f, size.y * 0.5f, size.z);
+			setClipModel = true;
+		}
 	}
 
-	if ( !collisionModelManager->ModelFromTrm(CM_WORLD_MAP, clipModelName, trm, NULL) ) {
-		gameLocal.Error( "idMoveable '%s': cannot load collision model %s", name.c_str(), clipModelName.c_str() );
-		return;
+	if (setClipModel) {
+		trm.SetupBox(bounds);
+	}
+	else {
+		if (!clipModelName[0]) {
+			clipModelName = spawnArgs.GetString("model");		// use the visual model
+		}
+		clipModelName.BackSlashesToSlashes();
+
+		if (!collisionModelManager->TrmFromModel(gameLocal.GetMapName(), clipModelName, trm)) {
+			gameLocal.Error("idMoveable '%s': cannot load collision model %s", name.c_str(), clipModelName.c_str());
+			return;
+		}
 	}
 
 	// if the model should be shrinked
